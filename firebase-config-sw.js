41232bba493a4f4da0bd533e5cf36bd4d15b7c4b
Almond1114 <ryo.murakami012@gmail.com>
2026-07-04 20:53:13 +0900

    Update firebase-config-sw.js

--- firebase-config-sw.js
+++ firebase-config-sw.js
@@ -1,10 +1,11 @@
 // firebase-messaging-sw.js から読み込む公開設定です。
 // app-config.js の FIREBASE_CONFIG / FIREBASE_VAPID_KEY と同じ値を入れてください。
 self.GRADE2_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDBKyDO2V2fGVcpZtBTl2z1ihte291yBk0",
+  authDomain: "grade2-website.firebaseapp.com",
+  projectId: "grade2-website",
+  storageBucket: "grade2-website.firebasestorage.app",
+  messagingSenderId: "1030380979370",
+  appId: "1:1030380979370:web:78419d5b3f4c1c1d10bcd3",
+  measurementId: "G-NT27E9BTR2"
 };
